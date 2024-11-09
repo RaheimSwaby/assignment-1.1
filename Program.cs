e3283f49bb4d827aeed9f4e740c3b35915bac647
Rahei <Rahei@RAHEIMSPC>
2024-11-08 22:03:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            do
+            {
+                Console.WriteLine("enter first number");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                if (num1 == 0)
+                {
+                    Console.WriteLine("number must be greater than 0 try again");
+                }
+            }
+            while (num1 == 0);
+            Console.WriteLine("Enter your second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("these number are equal");
+            }
+            if (num1 != num2)
+            {
+                Console.WriteLine("these numbers are not equal");
+            }
+           
+
+                    
+                
+            
+             
+                
+            
+        }
+    }
+}
